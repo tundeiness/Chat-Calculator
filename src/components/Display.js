1085c6e7f6987dba5edf114409d92d7b78c1fb21
tundeiness <tundeiness@yahoo.co.uk>
2020-11-03 19:08:45 +0100

    add prop-types to Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 const Display = props => {
   const { result } = props;
@@ -8,6 +8,10 @@ const Display = props => {
       <p className="inner-result">{result}</p>
     </div>
   );
+};
+
+Display.propTypes = {
+  result: PropTypes.string.isRequired,
 };
 
 export default Display;
